79bdc55c911ea7127398fbd0c9318f35dadf3948
Ari Bobesh <ari.bobesh@gmail.com>
2024-12-02 23:24:35 -0500

    Add some starter code. Does not build.

---
+++ neato-temple-run/main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+""" This is the starter code for the robot localization project """
+
+import rclpy
+from threading import Thread
+from rclpy.time import Time
+from rclpy.node import Node
+from std_msgs.msg import Header
+from sensor_msgs.msg import LaserScan
+from nav2_msgs.msg import ParticleCloud, Particle
+from nav2_msgs.msg import Particle as Nav2Particle
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
+from rclpy.duration import Duration
+import math
+import time
+import random
+import numpy as np
+from occupancy_field import OccupancyField
+from rclpy.qos import qos_profile_sensor_data
+import scipy.stats as sp
+
+
+class ParticleFilter(Node):
+    """ The class that represents a Particle Filter ROS Node
+        Attributes list:
+            base_frame: the name of the robot base coordinate frame (should be "base_footprint" for most robots)
+            map_frame: the name of the map coordinate frame (should be "map" in most cases)
+            odom_frame: the name of the odometry coordinate frame (should be "odom" in most cases)
+            scan_topic: the name of the scan topic to listen to (should be "scan" in most cases)
+            n_particles: the number of particles in the filter
+            d_thresh: the amount of linear movement before triggering a filter update
+            a_thresh: the amount of angular movement before triggering a filter update
+            pose_listener: a subscriber that listens for new approximate pose estimates (i.e. generated through the rviz GUI)
+            particle_pub: a publisher for the particle cloud
+            last_scan_timestamp: this is used to keep track of the clock when using bags
+            scan_to_process: the scan that our run_loop should process next
+            occupancy_field: this helper class allows you to query the map for distance to closest obstacle
+            transform_helper: this helps with various transform operations (abstracting away the tf2 module)
+            particle_cloud: a list of particles representing a probability distribution over robot poses
+            current_odom_xy_theta: the pose of the robot in the odometry frame when the last filter update was performed.
+                                   The pose is expressed as a list [x,y,theta] (where theta is the yaw)
+            thread: this thread runs your main loop
+    """
+    def __init__(self):
+        super().__init__('pf')
+        self.base_frame = "base_footprint"   # the frame of the robot base
+        self.map_frame = "map"          # the name of the map coordinate frame
+        self.odom_frame = "odom"        # the name of the odometry coordinate frame
+        self.scan_topic = "scan"        # the topic where we will get laser scans from 
+
+        # pose_listener responds to selection of a new approximate robot location (for instance using rviz)
+        self.create_subscription(PoseWithCovarianceStamped, 'initialpose', self.update_initial_pose, 10)
+
+        # publish the current particle cloud.  This enables viewing particles in rviz.
+        self.particle_pub = self.create_publisher(ParticleCloud, "particle_cloud", qos_profile_sensor_data)
+
+        # laser_subscriber listens for data from the lidar
+        self.create_subscription(LaserScan, self.scan_topic, self.scan_received, 10)
+
+        # this is used to keep track of the timestamps coming from bag files
+        # knowing this information helps us set the timestamp of our map -> odom
+        # transform correctly
+        self.last_scan_timestamp = None
+        # this is the current scan that our run_loop should process
+        self.scan_to_process = None
+        # your particle cloud will go here
+        self.particle_cloud = []
+
+        self.current_odom_xy_theta = []
+        self.occupancy_field = OccupancyField(self)
+
+        # we are using a thread to work around single threaded execution bottleneck
+        thread = Thread(target=self.loop_wrapper)
+        thread.start()
+        self.transform_update_timer = self.create_timer(0.05, self.pub_latest_transform)
+
+        # PARAMETERS
+        self.n_particles = 300          # the number of particles to use
+        self.d_thresh = 0.2             # the amount of linear movement before performing an update
+        self.a_thresh = math.pi/6       # the amount of angular movement before performing an update
+
+    def loop_wrapper(self):
+        """ This function takes care of calling the run_loop function repeatedly.
+            We are using a separate thread to run the loop_wrapper to work around
+            issues with single threaded executors in ROS2 """
+        while True:
+            self.run_loop()
+            time.sleep(0.1)
+
+    def run_loop(self):
+        """ This is the main run_loop of our particle filter.  It checks to see if
+            any scans are ready and to be processed and will call several helper
+            functions to complete the processing.
+            
+            You do not need to modify this function, but it is helpful to understand it.
+        """
+        print("main loop")
+            
+
+def main(args=None):
+    rclpy.init()
+    n = ParticleFilter()
+    rclpy.spin(n)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
